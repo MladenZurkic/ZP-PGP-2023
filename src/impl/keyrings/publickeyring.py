f3de93bffb2b89c0e647784ade9dfcc5b106216e
Filip <filip.stojsavljevic@gmail.com>
2023-05-24 21:46:00 +0200

    Update publickeyring.py

--- src/impl/keyrings/publickeyring.py
+++ src/impl/keyrings/publickeyring.py
@@ -1,13 +1,17 @@
+import binascii
 import time
 
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
 
-# Class used to store all values of one entry in Public Keyring
+
 class PublicKeyringValues:
-    def __init__(self, keyID, publicKey, userID):
+    def __init__(self, keyID, publicKey, userID, usedAlgorithm):
         self.timestamp = time.time()
         self.keyID = keyID
         self.publicKey = publicKey
         self.userID = userID
+        self.usedAlgorithm = usedAlgorithm
 
 
 class PublicKeyring:
@@ -15,10 +19,37 @@ class PublicKeyring:
     def __init__(self):
         self.publicKeyring = {}
 
-    def generateKey(self, name, email, password, algorithm, keySize):
-        pass
+    # Insert received public key
+    def insertKey(self, publicKey, userID, usedAlgorithm):
+        newKeyIDbin = publicKey.public_bytes(
+            encoding=serialization.Encoding.DER,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
 
+        newKeyID = int(binascii.hexlify(newKeyIDbin), 16) & ((1 << 64) - 1)
+        newPublicKey = PublicKeyringValues(
+            keyID=newKeyID,
+            publicKey=publicKey,
+            userID=userID,
+            usedAlgorithm=usedAlgorithm
+        )
+
+        self.publicKeyring[newKeyID] = newPublicKey
+
+    # Get key that's already in the keyring
     def getKey(self, keyID):
-        return self.publicKeyring[keyID]
+        try:
+            return self.publicKeyring[keyID]
+        except KeyError as err:
+            print('Unos u tabeli s vrednoscu ' + str(err.args[0]) + ' ne postoji...')
+            return None
 
-    
+
+
+# Test classes...
+if __name__ == '__main__':
+    pk = PublicKeyring()
+    key = rsa.generate_private_key(65537, 512).public_key()
+    pk.insertKey(publicKey=key, userID='abcd', usedAlgorithm='RSA')
+
+    pk.getKey(132465)  # Should report error
