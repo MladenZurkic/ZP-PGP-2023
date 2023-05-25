0b901cf6e2812192307083bb8553e3b899537dd1
Filip Stojsavljevic <101733446+filipStojsa@users.noreply.github.com>
2023-05-25 14:56:36 +0200

    Added insertKey(). Testing needed...

--- src/impl/keyrings/publickeyring.py
+++ src/impl/keyrings/publickeyring.py
@@ -3,6 +3,7 @@ import time
 
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
 
 
 class PublicKeyringValues:
@@ -19,14 +20,19 @@ class PublicKeyring:
     def __init__(self):
         self.publicKeyring = {}
 
-    # Insert received public key
-    def insertKey(self, publicKey, userID, usedAlgorithm):
+    def getKeyID(self, publicKey):
         newKeyIDbin = publicKey.public_bytes(
             encoding=serialization.Encoding.DER,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         )
 
-        newKeyID = int(binascii.hexlify(newKeyIDbin), 16) & ((1 << 64) - 1)
+        return int(binascii.hexlify(newKeyIDbin), 16) & ((1 << 64) - 1)
+
+    # Insert received public key
+    def insertKey(self, publicKey, userID, usedAlgorithm):
+        # Create new key...
+        newKeyID = self.getKeyID(publicKey)
+
         newPublicKey = PublicKeyringValues(
             keyID=newKeyID,
             publicKey=publicKey,
@@ -44,7 +50,36 @@ class PublicKeyring:
             print('Unos u tabeli s vrednoscu ' + str(err.args[0]) + ' ne postoji...')
             return None
 
+    # Used to export key to .pem file
+    def exportKey(self):
+        pass
 
+    # Used to import key from .pem file
+    # TEST NEEDED AFTER IMPLEMENTATION OF exportKey
+    def importKey(self, filename):
+        with open(filename, 'r') as file:
+            dat = file.read()
+            userId = dat.split('~')[0]
+            mail = dat.split('~')[2]  # Don't know about this...
+            usedAlgorithm = dat.split('~')[3]
+            publicKey = load_pem_public_key(dat.split('~')[4].encode('utf-8'))
+            keyID = self.getKeyID(publicKey)
+
+            newPublicKey = PublicKeyringValues(
+                keyID=keyID,
+                publicKey=publicKey,
+                userID=userId,
+                usedAlgorithm=usedAlgorithm
+            )
+
+            self.publicKeyring[keyID] = newPublicKey
+
+    # Remove key from keyring
+    def removeKey(self, keyID):
+        try:
+            del self.publicKeyring[keyID]
+        except KeyError as err:
+            print('Nije moguce obrisati kljuc s vrednoscu ' + str(err.args[0]))
 
 # Test classes...
 if __name__ == '__main__':
